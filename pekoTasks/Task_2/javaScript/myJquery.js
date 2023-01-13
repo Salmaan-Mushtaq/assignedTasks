f1887e89ad81ddfc8fdd88be856d4e96c24f0a1f
SalmaanMushtaq <salmaan@peko.one>
2023-01-13 09:22:31 +0530

    performed client-side validations through Jquery

--- pekoTasks/Task_2/javaScript/myJquery.js
+++ pekoTasks/Task_2/javaScript/myJquery.js
@@ -1,11 +1,43 @@
 $(document).ready(function () {
-   $('.form-details').submit(function(e){
-    e.preventDefault();
-    const values = $(this).serialize();
-    console.log(values);
+   $('.form-details').submit(function (e) {
+      e.preventDefault();
+      const values = $(this).serialize();
+      console.log(values);
    })
 
-   $('.btnReset').click(function(){
-    $('.form-details')[0].reset()
+   $('.form-details').validate({
+      errorClass: "alertFail",
+      validClass: "valid alertSuccess",
+      errorElement:"label",
+      wrapper:"p",
+
+      rules: {
+         firstname: 'required',
+         lastname: 'required',
+         designation: 'required',
+         mobile: {
+            required: true,
+            number: true,
+            maxlength: 12,
+            minlength: 10
+         },
+         email: {
+            required: true,
+            email: true,
+         }
+      },
+      messages: {
+         firstname: "Please enter first name",
+         lastname: "Please enter last name",
+         designation: "Please enter your designation",
+         mobile: "The mobile no should be 10-12 digits",
+         email: "The email should be : abc@domain.tld"
+      }
+   });
+
+   $('.btnReset').click(function (e) {
+      $('.form-details')[0].reset();
+      $('label.alertFail').hide();
+      e.preventDefault();
    });
 });
