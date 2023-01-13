0af60c7888695f5562a2a47b5075dfeab007fb58
SalmaanMushtaq <salmaan@peko.one>
2023-01-13 18:46:10 +0530

    adding some others way of getting the data from the form fields

--- pekoTasks/Task_2/javaScript/myJquery.js
+++ pekoTasks/Task_2/javaScript/myJquery.js
@@ -1,15 +1,10 @@
 $(document).ready(function () {
-   $('.form-details').submit(function (e) {
-      e.preventDefault();
-      const values = $(this).serialize();
-      console.log(values);
-   })
 
+   /*--- Form Validation Start ---*/
    $('.form-details').validate({
       errorClass: "alertFail",
       validClass: "valid alertSuccess",
-      errorElement:"label",
-      wrapper:"p",
+      errorElement: "label",
 
       rules: {
          firstname: 'required',
@@ -26,6 +21,7 @@ $(document).ready(function () {
             email: true,
          }
       },
+      /*--- Below Code is for Custom Messages ---*/
       messages: {
          firstname: "Please enter first name",
          lastname: "Please enter last name",
@@ -35,9 +31,54 @@ $(document).ready(function () {
       }
    });
 
+   /*--- Form Data Shown To Console Start ---*/
+   //The below code to console the form data as Object
+   $('.form-details').submit(function (e) {
+      e.preventDefault();
+      const values = {
+         firstname: $('#firstname').val(),
+         lastname: $('#lastname').val(),
+         designation: $('#designation').val(),
+         mobile: $('#mobile').val(),
+         email: $('#email').val(),
+         company: $('#company').val(),
+         city: $('#city').val(),
+         businessActivity: $('#businessActivity').val()
+
+      };
+      console.log(values);
+   });
+   /*--- Form Data Shown To Console End ---*/
+
+   /*--- Form Console Data Starts Here ---*/
+   /*--- The below code is to console the form data as encoded text string. ---*/
+   // $('.form-details').submit(function (e) {
+   //    e.preventDefault();
+   //    const values = $(this).serialize();
+   //    console.log(values);
+   // });
+   /*--- Form Console Data Ends Here ---*/
+
+   /*--- Other way of getting the form data as Array --*/
+   /*-- Code Start --*/
+
+   //  $('.form-details').submit(function (e) {
+   //    e.preventDefault();
+   //    const values = $(this).serializeArray();
+   //    console.log(values);
+   // });
+
+   /*-- Code End --*/
+
+   /*--- Form Reset Code Starts Here ---*/
+   //The below code is to reset the form details
+
    $('.btnReset').click(function (e) {
       $('.form-details')[0].reset();
       $('label.alertFail').hide();
       e.preventDefault();
    });
+
+   /*--- Form Reset Code Ends Here ---*/
+
 });
